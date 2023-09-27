2672b40befe3b7b5adb87dda9ebe3ec4bd99c80d
dhananjaya5023 <dhananjaya5023@gmail.com>
2023-09-27 16:38:26 -0400

    date

---
+++ date.js
@@ -0,0 +1,15 @@
+document.write("<h2>Javascript new Dtae</h2>");
+var d=new Date();
+document.write(d);
+document.write("<br/>");
+document.write("Date :"+d.getDate());
+document.write("<br/>");
+document.write("Day :"+d.getDay());
+document.write("<br/>");
+document.write("Year :"+d.getFullYear());
+document.write("<br/>");
+document.write("Hours :"+d.getHours());
+document.write("<br/>");
+document.write("minutes : "+d.getMinutes());
+document.write("<br/>");
+document.write("Seconds : "+d.getSeconds());
